9741f1396bee60ae0f24a1fb8b0e092ef9e2d1f0
Subtsirel <101670854+Subtsirel@users.noreply.github.com>
2022-05-07 17:40:43 +0300

    9.19

---
+++ LP9-main/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace LP9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.step = new System.Windows.Forms.TextBox();
+            this.textBoxXmax = new System.Windows.Forms.TextBox();
+            this.textBoxXmin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.LightGray;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Top;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "f(x1)";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "f(x2)";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1067, 462);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart2";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // step
+            // 
+            this.step.Location = new System.Drawing.Point(213, 482);
+            this.step.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.step.Name = "step";
+            this.step.Size = new System.Drawing.Size(175, 22);
+            this.step.TabIndex = 13;
+            // 
+            // textBoxXmax
+            // 
+            this.textBoxXmax.Location = new System.Drawing.Point(20, 530);
+            this.textBoxXmax.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxXmax.Name = "textBoxXmax";
+            this.textBoxXmax.Size = new System.Drawing.Size(175, 22);
+            this.textBoxXmax.TabIndex = 11;
+            // 
+            // textBoxXmin
+            // 
+            this.textBoxXmin.Location = new System.Drawing.Point(20, 482);
+            this.textBoxXmin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxXmin.Name = "textBoxXmin";
+            this.textBoxXmin.Size = new System.Drawing.Size(175, 22);
+            this.textBoxXmin.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(213, 528);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(204, 462);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1, 172);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(3, 460);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1067, 1);
+            this.panel2.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(1067, 608);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.step);
+            this.Controls.Add(this.textBoxXmax);
+            this.Controls.Add(this.textBoxXmin);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LP9";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox step;
+        private System.Windows.Forms.TextBox textBoxXmax;
+        private System.Windows.Forms.TextBox textBoxXmin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ LP9-main/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LP9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxXmin.Text = "0,12";
+            textBoxXmax.Text = "0,64";
+            step.Text = "0,2";
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double xmin = double.Parse(textBoxXmin.Text);
+            double xmax = double.Parse(textBoxXmax.Text);
+            double dx = double.Parse(step.Text);
+
+            int count = (int)Math.Ceiling((xmax - xmin) /dx) + 1;
+            double[] x = new double[count];
+            double[] y1 = new double[count];
+            double[] y2 = new double[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = xmin + dx * i;
+                y1[i] = Math.Pow(Math.Log(Math.Sin(Math.Pow(x[i], 3) + 0.0025)), 1.5) + (0.8 * Math.Pow(10, -3));
+                y2[i] = Math.Cos(x[i]);
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = xmax;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = dx;
+            chart1.Series[0].Points.DataBindXY(x, y1);
+            chart1.Series[1].Points.DataBindXY(x, y2);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
